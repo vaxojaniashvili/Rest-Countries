aeb5e2526f9501137e447d28a133ed98bdd4422b
vaxo <vaxojaniashvili186@gmail.com>
2024-03-27 13:29:11 +0400

    working-on-georgia

--- src/components/countries/CountriesInfo/CountriesInfo.tsx
+++ src/components/countries/CountriesInfo/CountriesInfo.tsx
@@ -1,8 +1,51 @@
-const CountriesInfo = () => {
-  return (
-    
-    <div>CountriesInfo</div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default CountriesInfo
+const GeorgiaInfo: React.FC = () => {
+  const [georgiaData, setGeorgiaData] = useState<any | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/name/georgia");
+        const [georgia] = await res.json();
+        setGeorgiaData(georgia);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const navigateToCountry = () => {
+    if (georgiaData) {
+      navigate(`/country/${georgiaData.name.common}`, {
+        state: georgiaData,
+      });
+    }
+  };
+
+  return (
+    <div>
+      {georgiaData && (
+        <div onClick={navigateToCountry}>
+          <button>
+            <Link to="/">Back</Link>
+          </button>
+          <h3>{georgiaData.name.common}</h3>
+          <img
+            src={georgiaData.flags.png}
+            alt={georgiaData.name.common}
+            style={{ width: "50px", height: "auto" }}
+          />
+          <p>Population: {georgiaData.population}</p>
+          <p>Map: {georgiaData.maps.googleMaps}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GeorgiaInfo;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CountriesInfo from "./components/countries/CountriesInfo/CountriesInfo.tsx";
+import Header from "./components/header/Header.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "country/georgia",
+    element: <>
+    <Header/>
+    <CountriesInfo/>
+    </>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Router>
-    <App />
-  </Router>
+  <RouterProvider router={router} />
 );
