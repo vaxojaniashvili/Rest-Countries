65ea46131aad2887665b1b9b8d2dfe67051af83b
vaxo <vaxojaniashvili186@gmail.com>
2024-03-27 14:23:17 +0400

    error

--- src/components/countries/CountriesInfo/CountriesInfo.tsx
+++ src/components/countries/CountriesInfo/CountriesInfo.tsx
@@ -28,20 +28,26 @@ const GeorgiaInfo: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="border border-black">
       {georgiaData && (
         <div onClick={navigateToCountry}>
           <button>
             <Link to="/">Back</Link>
           </button>
-          <h3>{georgiaData.name.common}</h3>
-          <img
-            src={georgiaData.flags.png}
-            alt={georgiaData.name.common}
-            style={{ width: "50px", height: "auto" }}
-          />
-          <p>Population: {georgiaData.population}</p>
-          <p>Map: {georgiaData.maps.googleMaps}</p>
+          <div className="flex gap-8 justify-center">
+            <img src={georgiaData.flags.png} />
+            <div className="block">
+              <h3>{georgiaData.name.common}</h3>
+              <h3 className="flex gap-1">
+                Native Name:
+                <div className="text-[#111517]">{georgiaData.name.common}</div>
+              </h3>
+              <p className="flex gap-1 font-headerFont">Population:
+                <div className="text-[#111517]">{georgiaData.population}</div>
+              </p>
+
+            </div>
+          </div>
         </div>
       )}
     </div>
